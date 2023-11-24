fb711507ee0eb4a5a61058b4bfb484f1610f5688
noha2002 <105394630+noha2002@users.noreply.github.com>
2023-11-24 16:04:55 +0200

    create tables

---
+++ database/migrations/2023_11_23_212935_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->enum('role', ['Roles']); //roles have not been determined yet
+            $table->enum('department', ['Financial','Managerial','Worker','TeachingStuff']); 
+            $table->integer('basic_salary');
+            $table->unsignedBigInteger('subject_id')->nullable()->default(null);
+            $table->foreign('subject_id')->references('id')->on('subjects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
---
+++ database/migrations/2023_11_23_213157_create_salaries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('month');
+            $table->integer('year');
+            $table->integer('basic_salary');
+            $table->integer('bonus')->default(0);
+            $table->integer('deductions')->default(0);
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
