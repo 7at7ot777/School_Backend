e36a315527251ff42a9cd761d61e693570539480
xX7at7otXx <mohamedhathout3@gmail.comgit config --global user.email mohamedhathout3@gmail.com>
2023-12-13 02:28:12 +0200

    change return status from 0 1 to true false

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -79,7 +79,7 @@ class AdminController extends Controller
                 'avatarUrl' => '', // Add logic to get the avatar URL if available
                 'name' => $item['user']['name'],
                 'email' => $item['user']['email'],
-                'status' => $item['user']['status'], // You can customize the status based on your criteria
+                'status' => $item['user']['status'] == 0 ? false : true, // You can customize the status based on your criteria
                 'department' => [
                     'id' => $item['department']['id'],
                     'name' => $item['department']['name'],
