f64367111bc1f6fcc11b641fdb5c08dcb6c2196d
7at7ot777 <mohamedhathout3@gmail.com>
2024-02-02 03:40:41 +0200

    Import Admins using excel sheet

---
+++ app/Imports/ImportAdmin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Department;
+use App\Models\Employee;
+use App\Models\User;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class ImportAdmin implements ToModel, WithHeadingRow
+{
+    /**
+     * @param Collection $collection
+     */
+
+    public $counter = 0;
+
+    public function __construct()
+    {
+        $counter = 0;
+    }
+
+    public function model(array $row)
+    {
+//        dd($row);
+        if(!isset($row['name']))
+            return null;
+        // Create a new User
+        $newUser = new User([
+            'name' => $row['name'] ?? ' ',
+            'phone' => $row['phone']?? ' ',
+            'address' => $row['address']?? ' ',
+            'password' => bcrypt('welcome'),
+            'email' => $row['email'] ?? ' ',
+            'user_type' => 'employee',
+        ]);
+        $newUser->save();
+
+        // Create a new Employee
+        $newEmployee = new Employee([
+            'user_id' => $newUser->id,
+            'department_id' => Department::where('name','LIKE', $row['department'])->first()->id ?? null,
+            'basic_salary' => $row['basic_salary'] ?? null,
+            'role' => 'admin',
+            'subject_id' => null, // You might need to adjust this based on your actual data
+        ]);
+        $newEmployee->save();
+
+        return $newEmployee;
+
+
+    }
+}
